b32a17341a90d12fec7da5dc1fc7f1efc1cf73dc
Lynzzyr <brandonnamgoong@gmail.com>
2026-04-10 09:40:43 -0400

    menu bar, revisions

--- src/script.js
+++ src/script.js
@@ -5,3 +5,14 @@ TEJ4M
 Cookie Clicker
 */
 
+// menu bar tooltip hovering
+for (const item of document.getElementsByClassName("menu-bar-item")) {
+    // when hovering
+    item.addEventListener("mouseenter", () => {
+        document.getElementById("menu-bar-tooltip").style.visibility = "visible";
+    });
+    // when not
+    item.addEventListener("mouseleave", () => {
+        document.getElementById("menu-bar-tooltip").style.visibility = "hidden";
+    });
+}
