e37f6fa65e5386e01e15a56697840a15019014a6
Lynzzyr <brandonnamgoong@gmail.com>
2026-04-13 01:51:05 -0400

    all functionality working

--- src/script.js
+++ src/script.js
@@ -1,32 +1,64 @@
 /*
 Brandon Namgoong
 TEJ4M
-2026-04-##
+2026-04-13
 Cookie Clicker
 */
 
-// globals
+// variables
 
 let cookies = 0;
 let perclick = 1;
-let persecond = 0.5;
+let persecond = 0.5; // DEV NOTE DONT LET THIS BE 0
+let upgradecostPerclick = 50;
+let upgradecostPersecond = 200;
 
 let elCookies = document.getElementById("stat-cookies");
 let elPerclick = document.getElementById("stat-perclick");
 let elPersecond = document.getElementById("stat-persecond");
+let elUpgradecostPerclick = document.getElementById("upgradecost-perclick");
+let elUpgradecostPersecond = document.getElementById("upgradecost-persecond");
+
+let cookieInterval = constructCookieInterval();
 
 // functions
 
-/** Add to current cookie count by per the current number of cookies per click. */
-function changeCookiesByClick() {
-    cookies += perclick;
+/** Update number of cookies. */
+function updateCookies() {
     elCookies.innerHTML = cookies;
 }
 
-/** Add to or subtract from current cookie count. Subtract by setting `amount` to a negative number. */
+/** Update number of cookies per click. */
+function updatePerclick() {
+    elPerclick.innerHTML = perclick;
+}
+
+/** Update number of cookies per second. */
+function updatePersecond() {
+    elPersecond.innerHTML = persecond;
+}
+
+/** Adds to current cookie count by per the current number of cookies per click. */
+function changeCookiesByClick() {
+    cookies += perclick;
+    updateCookies();
+}
+
+/** Adds to or subtracts from current cookie count. Subtract by setting `amount` to a negative number. */
 function changeCookiesBy(amount) {
     cookies += amount;
-    elCookies.innerHTML = cookies;
+    updateCookies();
+}
+
+/** Adds 1 to the number of cookies per click only if possible with current number of cookies.*/
+function incrementCookiesPerClick() {
+    if (cookies >= upgradecostPerclick) { // cost check
+        perclick++;
+        updatePerclick();
+
+        cookies -= upgradecostPerclick; // subtract upgrade cost
+        updateCookies();
+    }
 }
 
 /** Convenience function that creates and returns a new cookie interval based on the current cookies per second. */
@@ -39,9 +71,20 @@ function constructCookieInterval() {
     );
 }
 
-// variables
+/** Doubles the number of cookies per second only if possible with current number of cookies. */
+function doubleCookieInterval() {
+    if (cookies >= upgradecostPersecond) { // cost check
+        persecond *= 2;
+        updatePersecond()
 
-let cookieInterval = constructCookieInterval();
+        cookies -= upgradecostPersecond; // subtract upgrade cost
+        updateCookies();
+
+        // reconstruct interval
+        clearInterval(cookieInterval);
+        cookieInterval = constructCookieInterval();
+    }
+}
 
 // page setup
 
@@ -58,6 +101,9 @@ for (const item of document.getElementsByClassName("menu-bar-item")) {
 }
 
 // stats bar start values
-elCookies.innerHTML = cookies;
-elPerclick.innerHTML = perclick;
-elPersecond.innerHTML = persecond;
+updateCookies();
+updatePerclick();
+updatePersecond();
+
+elUpgradecostPerclick.innerHTML = upgradecostPerclick;
+elUpgradecostPersecond.innerHTML = upgradecostPersecond;
