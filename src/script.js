8897694a4e65e14296d06bed8f0a4e238b905e9f
Lynzzyr <brandonnamgoong@gmail.com>
2026-04-13 00:21:26 -0400

    basic clicking

--- src/script.js
+++ src/script.js
@@ -5,6 +5,32 @@ TEJ4M
 Cookie Clicker
 */
 
+// globals
+
+let cookies = 0;
+let perclick = 1;
+let persecond = 0;
+
+let elCookies = document.getElementById("stat-cookies");
+let elPerclick = document.getElementById("stat-perclick");
+let elPersecond = document.getElementById("stat-persecond");
+
+// functions
+
+/** Add to current cookie count by per the current number of cookies per click. */
+function changeCookiesByClick() {
+    cookies += perclick;
+    elCookies.textContent = cookies;
+}
+
+/** Add to or subtract from current cookie count. Subtract by setting `amount` to a negative number. */
+function changeCookies(amount) {
+    cookies += amount;
+    elCookies.textContent = cookies;
+}
+
+// document setup
+
 // menu bar tooltip hovering
 for (const item of document.getElementsByClassName("menu-bar-item")) {
     // when hovering
@@ -16,3 +42,8 @@ for (const item of document.getElementsByClassName("menu-bar-item")) {
         document.getElementById("menu-bar-tooltip").style.visibility = "hidden";
     });
 }
+
+// stats bar start values
+elCookies.textContent = cookies;
+elPerclick.textContent = perclick;
+elPersecond.textContent = persecond;
